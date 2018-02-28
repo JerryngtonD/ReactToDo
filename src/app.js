32bac28615ed38805218659c436bf9b5aa0950c8
jerryngton <jerryngtond@gmail.com>
2018-02-28 16:45:15 +0300

    input and listItem were added as components

--- src/app.js
+++ src/app.js
@@ -1,13 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//import App from './components/App';
-import Button from './components/Button';
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/header';
+import ToDoInput from './components/todoInput';
+import TodoItem from './components/todoItem';
 
-const ENTRY_NODE = document.getElementById('app');
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(
-  <Button color="red">
-    <h1>This is Sparta</h1>
-  </Button>,
-  ENTRY_NODE,
-);
+    this.state = {
+      todos: [
+        { id: 0, text: 'Make dinner tonight!' },
+        { id: 1, text: 'Fold the laundry' },
+        { id: 2, text: 'Take a picture' },
+      ],
+      nextId: 3,
+    };
+
+    this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+  }
+
+  addTodo(todoText) {
+    let todos = this.state.todos.slice();
+    todos.push({ id: this.state.nextId, text: todoText });
+    this.setState({
+      todos: todos,
+      nextId: ++this.state.nextId,
+    });
+  }
+
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter((todo, index) => todo.id !== id)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="todo-wrapper">
+          <Header />
+          <ToDoInput todoText="" addTodo={this.addTodo} />
+          <ul>
+            {this.state.todos.map(todo => {
+              return (
+                <TodoItem
+                  todo={todo}
+                  key={todo.id}
+                  id={todo.id}
+                  removeTodo={this.removeTodo}
+                />
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/components/Button.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Button(props) {
-  return <button style={{ color: props.color }}>{props.children}</button>;
-}
